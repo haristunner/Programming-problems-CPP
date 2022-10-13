e38d59622c47c31825f7cec67854a802e58000b0
hari <103772225+haristunner@users.noreply.github.com>
2022-10-13 22:21:08 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,26 +1,17 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int n;
+    int a=0,b=1,n,c=0;
     cin>>n;
-    int c;
-    int a=0,b=1;
-    if(n==0)
-     cout<<"no";
-    else if(n==1)
-     cout<<a;
-    else
-    {cout<<a<<" ";
-    cout<<b<<" ";}
-    int sum=b;
-    while((a+b)<10)
+    cout<<a<<" "<<b<<" ";
+    for(int i=2; i<n; i++)
     {
         c=a+b;
+        cout<<c<<" ";
         a=b;
         b=c;
-        sum+=c;
-        cout<<c<<" "<<endl;
     }
-    cout<<sum;
+    return 0;
 }
+
