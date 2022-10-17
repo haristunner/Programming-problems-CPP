7454ae1e5256ca94e53a1f9f9c7e329ece392151
hari <103772225+haristunner@users.noreply.github.com>
2022-10-17 22:56:40 +0530

    Create remove_duplicates_arr.cpp

---
+++ remove_duplicates_arr.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int s[n];
+    for(int i=0; i<n; i++)
+        cin>>s[i];
+    
+    for(int i=0; i<n; i++)
+    {   
+        for(int j=i+1; j<n; j++)
+        {
+            if(s[i]==s[j])
+            {
+                for(int k=j; k<n; k++)
+                {
+                    s[k]=s[k+1];
+                }
+                j--;
+                n--;
+            }
+        }
+    }
+    for(int i=0; i<n; i++)
+        cout<<s[i]<<" ";
+    return 0;
+}
