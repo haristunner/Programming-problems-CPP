7693be36ef2476319d5334e651ee976ac8203c5b
hari <103772225+haristunner@users.noreply.github.com>
2022-11-02 23:05:15 +0530

    Create kadane's_algorithm.cpp

--- kadane's_algorithm.cpp
+++ kadane's_algorithm.cpp
@@ -5,20 +5,25 @@ int main()
 {
     int n;
     cin>>n;
-    int arr[n];
+    int a[n];
     for(int i=0; i<n; i++)
-     cin>>arr[i];
-    int sum=0,max=INT_MIN;
+        cin>>a[i];
+    int start=0,end=0;
+    int sum=0, max=INT_MIN;
     for(int i=0; i<n; i++)
-    {
-        sum+=arr[i];
-        if(sum>max)
-        {
-            max=sum;
-        }
-        if(sum<0)
-        sum=0;
+    {   
+       sum+=a[i];    
+       if(sum>max){
+       max=sum;
+       end=i;
     }
+    if(sum<0){
+        sum=0;
+        start=i+1;
+    }}
+    for(int i=start; i<=end; i++)
+        cout<<a[i]<<" ";
+    cout<<endl;    
     cout<<max;
     return 0;
 }
