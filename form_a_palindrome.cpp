6596acb8ffb6cda697b1929380a859e3e8a3150f
hari <103772225+haristunner@users.noreply.github.com>
2022-11-30 22:46:03 +0530

    Create form_a_palindrome.cpp
    https://practice.geeksforgeeks.org/problems/form-a-palindrome1455/1?page=2&status[]=unsolved&category[]=Arrays&category[]=Strings&curated[]=1&sortBy=submissions

---
+++ form_a_palindrome.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s="aaaabaaaa";
+    int n=s.length();
+    string t="", r="";
+    int j=0, count=0, n1=0;
+    if(n%2==0)
+    {
+        for(int i=0; i<n/2; i++)
+        {
+            t+=s[i];
+            n1++;
+        }
+        for(int i=(n/2)-1; i>=0; i--)
+        {
+            r+=t[i];
+        }
+        for(int i=n/2; i<n; i++){
+            
+            if(r[j]==s[i]){
+                count++;
+                j++;
+            }
+            else
+                j++;
+        }
+        if(count==n1)
+        {
+        cout<<"0";
+        return 0;
+        }
+    }
+    else{
+        for(int i=0; i<(n/2)-1; i++)
+        {
+            t+=s[i];
+            n1++;
+        }
+        for(int i=(n/2)-2; i>=0; i--)
+        {
+            r+=t[i];
+        }
+        for(int i=(n/2)+1; i<n; i++){
+            
+            if(r[j]==s[i]){
+                count++;
+                j++;
+            }
+            else
+                j++;
+        }
+        if(count==n1)
+        {
+        cout<<"0";
+        return 0;
+        }
+    }
+    cout<<n-1;
+    return 0;
+}
