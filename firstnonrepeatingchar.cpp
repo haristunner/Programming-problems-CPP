d67312ce34e3db4c4fc5d6a4c50df37fe6451ac3
hari <103772225+haristunner@users.noreply.github.com>
2023-05-03 10:18:55 +0530

    Update firstnonrepeatingchar.cpp

--- firstnonrepeatingchar.cpp
+++ firstnonrepeatingchar.cpp
@@ -26,3 +26,24 @@ int main()
        cout<<ans;
     return 0;
 }
+
+// another method
+
+// string s;
+//     cin>>s;
+//     int n=s.length(),count=0;
+//     char c;
+    
+//     for(int i=0; i<n; i++){
+//         for(int j=i+1;j<n;j++){
+//             if(s[i]==s[j]){
+//                 count++;
+//             }
+//         }
+//         if(count==0){
+//             cout<<s[i];
+//             return 0;
+//         }
+//         // cout<<count;
+//         count=0;
+//     }
